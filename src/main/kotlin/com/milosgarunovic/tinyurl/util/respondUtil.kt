660c245ec3433fd1081f613184a3a62baec70f43
milos.garunovic <milos.garunovic@gmail.com>
2023-02-15 13:15:05 +0100

    minor refactor

--- src/main/kotlin/com/milosgarunovic/tinyurl/util/respondUtil.kt
+++ src/main/kotlin/com/milosgarunovic/tinyurl/util/respondUtil.kt
@@ -3,6 +3,6 @@ package com.milosgarunovic.tinyurl.util
 import io.ktor.http.*
 import io.ktor.server.application.*
 
-public suspend inline fun ApplicationCall.respondWithStatusCode(status: HttpStatusCode) {
+suspend inline fun ApplicationCall.respondWithStatusCode(status: HttpStatusCode) {
     response.status(status)
 }
