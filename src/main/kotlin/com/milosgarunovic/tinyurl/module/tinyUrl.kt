ca432443629821791208b6a0dc9eecbe76f465f1
milos.garunovic <milos.garunovic@gmail.com>
2023-02-16 14:40:13 +0100

    minor refactor

--- src/main/kotlin/com/milosgarunovic/tinyurl/module/tinyUrl.kt
+++ src/main/kotlin/com/milosgarunovic/tinyurl/module/tinyUrl.kt
@@ -4,7 +4,7 @@ import com.milosgarunovic.tinyurl.json.TinyUrlAddReq
 import com.milosgarunovic.tinyurl.json.TinyUrlUpdateReq
 import com.milosgarunovic.tinyurl.repository.InMemoryRepository
 import com.milosgarunovic.tinyurl.util.respondRedirect
-import com.milosgarunovic.tinyurl.util.respondWithStatusCode
+import com.milosgarunovic.tinyurl.util.respondStatusCode
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
@@ -21,7 +21,7 @@ fun Application.tinyUrl(repository: InMemoryRepository) {
                 if (url != null) {
                     call.respondRedirect(url) // TODO add statistics for that url
                 } else {
-                    call.respondWithStatusCode(HttpStatusCode.NotFound)
+                    call.respondStatusCode(HttpStatusCode.NotFound)
                 }
             }
         }
@@ -37,14 +37,14 @@ fun Application.tinyUrl(repository: InMemoryRepository) {
             patch {
                 val req = call.receive<TinyUrlUpdateReq>() // todo must be a valid url
                 repository.update(req.id, req.url)
-                call.respondWithStatusCode(HttpStatusCode.OK)
+                call.respondStatusCode(HttpStatusCode.OK)
             }
 
             delete("/{id}") {
                 val id = call.parameters["id"] ?: return@delete call.respond(HttpStatusCode.BadRequest)
 
                 repository.delete(id)
-                call.respondWithStatusCode(HttpStatusCode.NoContent)
+                call.respondStatusCode(HttpStatusCode.NoContent)
             }
         }
     }
--- src/main/kotlin/com/milosgarunovic/tinyurl/util/respondUtil.kt
+++ src/main/kotlin/com/milosgarunovic/tinyurl/util/respondUtil.kt
@@ -1,14 +1,15 @@
 package com.milosgarunovic.tinyurl.util
 
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 
-suspend inline fun ApplicationCall.respondWithStatusCode(status: HttpStatusCode) {
-    response.status(status)
+suspend inline fun ApplicationCall.respondStatusCode(status: HttpStatusCode) {
+    respond(status, NullBody)
 }
 
 suspend inline fun ApplicationCall.respondRedirect(url: String) {
-    respondWithStatusCode(HttpStatusCode.MovedPermanently)
+    response.status(HttpStatusCode.MovedPermanently)
     respondRedirect(url, true)
 }
