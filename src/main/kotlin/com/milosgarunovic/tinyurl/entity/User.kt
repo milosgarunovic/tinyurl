27c556dd122e4fe674bb01bc96fce81bc4286934
milos.garunovic <milos.garunovic@gmail.com>
2023-04-22 00:01:58 +0200

    remove isAdmin from User

--- src/main/kotlin/com/milosgarunovic/tinyurl/entity/User.kt
+++ src/main/kotlin/com/milosgarunovic/tinyurl/entity/User.kt
@@ -3,5 +3,5 @@ package com.milosgarunovic.tinyurl.entity
 data class User(
     val email: String,
     val password: String,
-    val isAdmin: Boolean,
+//    val isAdmin: Boolean, // for now it's only a field in DB and it's not exposed
 ) : BaseEntity()
