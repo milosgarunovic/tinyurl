b67dc7bc8c7428f7f999a4069cd51f21ebd4cecf
milos.garunovic <milos.garunovic@gmail.com>
2023-05-09 14:47:38 +0200

    ktor version update and added openAPI

--- src/main/kotlin/com/milosgarunovic/tinyurl/module/openApi.kt
+++ src/main/kotlin/com/milosgarunovic/tinyurl/module/openApi.kt
@@ -1,9 +1,11 @@
 package com.milosgarunovic.tinyurl.module
 
 import io.ktor.server.application.*
+import io.ktor.server.plugins.openapi.*
+import io.ktor.server.routing.*
 
 fun Application.openApi() {
-//    routing {
-//        openAPI(path = "openapi")
-//    }
+    routing {
+        openAPI(path = "openapi")
+    }
 }
