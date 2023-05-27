d7605eedb2535c7e9c293c5df0e554a663cbd58f
milos.garunovic <milos.garunovic@gmail.com>
2023-05-27 11:56:15 +0200

    move cio to netty engine because of ssl

--- src/main/kotlin/com/milosgarunovic/tinyurl/main.kt
+++ src/main/kotlin/com/milosgarunovic/tinyurl/main.kt
@@ -5,7 +5,7 @@ package com.milosgarunovic.tinyurl
 import com.milosgarunovic.tinyurl.module.*
 import com.milosgarunovic.tinyurl.repository.SQLite
 import io.ktor.server.application.*
-import io.ktor.server.cio.*
+import io.ktor.server.netty.*
 
 fun main(args: Array<String>) {
 
